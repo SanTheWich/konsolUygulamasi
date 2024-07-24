c6b1d82a1d60db95be2fb613dde5188f902145fd
talha <yasin_talha_@hotmail.com.tr>
2024-07-24 19:52:16 +0300

    hesap makinesi

--- hesap makinesi.py
+++ hesap makinesi.py
@@ -3,10 +3,28 @@ print("\033[1;32;40m")
 print("╔═════════════════════╗")
 print("║\033[1;31;40m   HESAP MAKİNESİ  \033[1;32;40m  ║")
 print("║                     ║")
-print("║  1-Toplama          ║")
-print("║  2-Çıkarma          ║")
-print("║  3-                 ║")
-print("║  4-                 ║")
+print("║  1-Toplama(+)       ║")
+print("║  2-Çıkarma(-)       ║")
+print("║  3-Bölme(/)         ║")
+print("║  4-Çarpma(*)        ║")
 print("║                     ║")
 print("║    Seçimiz nedir?   ║")
 print("╚═════════════════════╝")
+hesaplama=str(input())
+a=int(input("Lütfen ilk sayınızı giriniz: "))
+
+b=int(input("Lütfen ikinci sayınızı giriniz: "))
+toplama = '+'
+bolme = '/'
+carpma= '*'
+cikarma= '-'
+
+if hesaplama == toplama:
+    print("{} + {} = {}".format(a,b,a+b))
+    input()
+elif hesaplama == bolme:
+        print("{} / {} = {}".format(a,b,a/b))
+elif hesaplama == carpma:
+     print("{} * {} = {}".format(a,b,a*b))
+elif hesaplama == cikarma:
+    print("{} - {} = {}".format(a,b,a-b))
