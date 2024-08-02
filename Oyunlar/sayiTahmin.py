57e37d9b030b36dc6728d9d155236bad3c6d0172
talha <yasin_talha_@hotmail.com.tr>
2024-08-02 19:11:25 +0300

    ipucu eklemesi

--- Oyunlar/sayiTahmin.py
+++ Oyunlar/sayiTahmin.py
@@ -3,7 +3,7 @@ def sayiBilmeceOyna():
     sayi=(random.randint(1,10))
     hak=5
     puan=100
-    for a in range (3):
+    for a in range (5):
         tahmin=int(input("1 ile 10 arasindan sayi tahmin ediniz: "))
         if sayi==tahmin:
             print(f"Doğru bildiniz \n Puaniniz {puan}")
@@ -11,7 +11,10 @@ def sayiBilmeceOyna():
         elif hak>1:
             hak=hak-1
             puan=puan-10
-            print(f"Bilemediniz {hak} hakkiniz kaldi")
+            if sayi<tahmin:
+               print(f"Bilemediniz {hak} hakkiniz kaldi (Sayini küçült ↓)")
+            else:
+                print(f"Bilemediniz {hak} hakkiniz kaldi (Sayini büyült ↑)")
         else:
             print("Hakkin kalmadi kaybettin.")
 def main():
